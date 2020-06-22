c5e6d9344e690cfb595505b0db6fbd344d8a3f63
Stephin <stephinreji123@gmail.com>
2020-06-23 01:17:40 +0530

    Initial Commit :octocat:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      /* Uses a transparent header that draws on top of the layout's background */ 
+      < div style = {{ height: '300px', position: 'relative' }}>
+    <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover' }}>
+      <Header transparent title="Title" style={{ color: 'white' }}>
+        <Navigation>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+        </Navigation>
+      </Header>
+      <Drawer title="Title">
+        <Navigation>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+        </Navigation>
+      </Drawer>
+      <Content />
+    </Layout>
+</div >
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+//adding react mdl js/css
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
