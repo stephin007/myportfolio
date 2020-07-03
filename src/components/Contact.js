a8da8008ec78aa153c79b9ce06c58799ea55b818
Civil Machines Technologies Private Limited <stephin007>
2020-07-04 01:54:28 +0530

    Added Home page of the portfolio :octocat:

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Contact extends Component{
     render(){    
         return (
-                <h1>Contact Page</h1>
+                <h1>Contact Coming Soon</h1>
             )
         }    
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Portfolio extends Component{
     render(){    
         return (
-                <h1>Portfolio Page</h1>
+                <h1>Portfolio Coming Soon</h1>
             )
         }    
 }
