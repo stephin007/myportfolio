2ab64fac0a5d7186630249d19ff9a8cbe745be28
Civil Machines Technologies Private Limited <stephin007>
2020-07-04 23:46:52 +0530

    Some changes in the whatsapp link

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ class Home extends Component{
                                     <div className="social-links">
                                         <a href="https://github.com/stephin007" target="_blank" rel = "noopener noreferrer"><i class= "fa fa-github"></i></a>
                                         <a href="https://twitter.com/reji_stephin" target="_blank" rel = "noopener noreferrer"><i class= "fa fa-twitter"></i></a>
-                                        <a href="https://wa.me/918860102173?text=Interested%20to%20talk%20with%20you" target="_blank" rel = "noopener noreferrer"><i class= "fa fa-whatsapp"></i></a>
+                                        <a href="https://wa.me/918860102173?text=Would%20love%20to%20talk%20with%20you" target="_blank" rel = "noopener noreferrer"><i class= "fa fa-whatsapp"></i></a>
                                         <a href="https://www.facebook.com/stephin.reji1998" target="_blank" rel = "noopener noreferrer"><i class= "fa fa-facebook"></i></a>
                                         <a href="https://www.instagram.com/the_spectacled_one/?hl=en" target="_blank" rel = "noopener noreferrer"><i class= "fa fa-instagram" ></i></a>
                                     </div>
