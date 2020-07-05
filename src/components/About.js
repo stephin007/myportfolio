b9bb68277511561f1e35d48afebea5f4d012beb3
Civil Machines Technologies Private Limited <stephin007>
2020-07-06 02:02:16 +0530

    Added personal details and skills

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 import '../css/about.css'
 
@@ -18,6 +19,71 @@ class About extends Component{
                                     <div className="about-text padd-15 ">
                                         <h3>Communication is the <span>key</span></h3>
                                         <p>Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="personal-info padd-15">
+                                        <div className="row">
+                                            <div className="info-item padd-15">
+                                                <p>Birthday : <span>22 June 1998</span></p>
+                                            </div>
+                                            <div className="info-item padd-15">
+                                                <p>Age : <span>22</span></p>
+                                            </div>
+                                            <div className="info-item padd-15">
+                                                <p>Website : <span>www.stephinreji.me</span></p>
+                                            </div>
+                                            <div className="info-item padd-15">
+                                                <p>Email : <span>stephinreji123@gmail.com</span></p>
+                                            </div>
+                                            <div className="info-item padd-15">
+                                                <p>Degree : <span>B.Tech</span></p>
+                                            </div>
+                                            <div className="info-item padd-15">
+                                                <p>Phone : <span>+91 8920 788 383</span></p>
+                                            </div>
+                                            <div className="info-item padd-15">
+                                                <p>City : <span>Delhi</span></p>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="buttons padd-15">
+                                                <a href="https://www.google.com" className="bn">Download Resume</a>
+                                                <a href="https://www.google.com" className="bn bn-space">Hire Me</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="skills padd-15">
+                                        <div className="row">
+                                            <div className="skill-item padd-15">
+                                                <h5>Javascript</h5>
+                                                <div className="progress">
+                                                    <div className="progress-in" style= {{width: "40%"}}></div>
+                                                    <div className="skill-percent">40%</div>
+                                                </div>
+                                            </div>
+                                            <div className="skill-item padd-15">
+                                                <h5>React</h5>
+                                                <div className="progress">
+                                                    <div className="progress-in" style= {{width: "50%"}}></div>
+                                                    <div className="skill-percent">50%</div>
+                                                </div>
+                                            </div>
+                                            <div className="skill-item padd-15">
+                                                <h5>HTML</h5>
+                                                <div className="progress">
+                                                    <div className="progress-in" style= {{width: "80%"}}></div>
+                                                    <div className="skill-percent">80%</div>
+                                                </div>
+                                            </div>
+                                            <div className="skill-item padd-15">
+                                                <h5>CSS</h5>
+                                                <div className="progress">
+                                                    <div className="progress-in" style= {{width: "70%"}}></div>
+                                                    <div className="skill-percent">70%</div>
+                                                </div>
+                                            </div>
+                                        </div>   
                                     </div>
                                 </div>
                             </div>
