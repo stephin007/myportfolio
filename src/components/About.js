a7aeccc1da34c2e1f8144ac79d580211563efb94
Civil Machines Technologies Private Limited <stephin007>
2020-07-05 20:59:44 +0530

    checking the workflow

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ class About extends Component{
                                 <div className="row">
                                     <div className="about-text padd-15 ">
                                         <h3>Communication is the <span>key</span></h3>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                                        <p>Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
                                     </div>
                                 </div>
                             </div>
