af03e7a09ee98f1c29371cd42bdf0b50116fe062
ZenVega <50147356+ZenVega@users.noreply.github.com>
2020-10-12 22:33:02 +0200

    project data dynamic

---
+++ src/data/ProjectData.js
@@ -0,0 +1,27 @@
+const ProjectData = {
+  byID:{
+    1: {
+      title: "A Currency Convertor App||Made using ReactJS",
+      description: "App fetches the currency exchange rates of 33 countries",
+    },
+    2: {
+      title: "A Currency Convertor App||Made using ReactJS",
+      description: "App fetches the currency exchange rates of 33 countries",
+    },
+    3: {
+      title: "A Currency Convertor App||Made using ReactJS",
+      description: "App fetches the currency exchange rates of 33 countries",
+    },
+    4: {
+      title: "A Currency Convertor App||Made using ReactJS",
+      description: "App fetches the currency exchange rates of 33 countries",
+    },
+    5: {
+      title: "A Currency Convertor App||Made using ReactJS",
+      description: "App fetches the currency exchange rates of 33 countries",
+    },
+  },
+  allIDs:[1,2,3,4,5]
+
+}
+export default ProjectData
