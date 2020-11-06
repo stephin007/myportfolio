079f89ef16861fac6bafb32c9c39b8de906748cf
stephin007 <stephinreji123@gmail.com>
2020-11-06 08:18:20 +0530

    Added imgLink to the json

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,32 +1,43 @@
 const ProjectData = {
   byID:{
     1: {
-      title: "A Currency Convertor App||Made using ReactJS",
-      description: "App fetches the currency exchange rates of 33 countries",
-      link: "https://currencyconvertor.stephinreji.me/"
+      title: "The Git Searcher||Made using ReactJS",
+      description: "An App to search Github Users",
+      projLink: "https://gitsearcher.stephinreji.me/",
+      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627671/portfolio/gitsearcher.stephinreji.me__Pixel_2_ccp0ai.png"
     },
     2: {
-      title: "A Weather Application||Made using ReactJS",
-      description: "It uses an Open Weather API to fetch weather details",
-      link: "https://weatherman.stephinreji.me/"
+      title: "A Currency Convertor App||Made using ReactJS",
+      description: "App fetches the currency exchange rates of 33 countries",
+      link: "https://currencyconvertor.stephinreji.me/",
+      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627740/portfolio/currencyconvertor.stephinreji.me__Moto_G4_v5xir7.png"
     },
     3: {
-      title: "Photographer's Portfolio||Made using HTML/CSS",
-      description: "It's a fully customised Photographer portfolio",
-      link: "https://chitrahaar.stephinreji.me/"
+      title: "A Weather Application||Made using ReactJS",
+      description: "It uses an Open Weather API to fetch weather details",
+      projLink: "https://weatherman.stephinreji.me/",
+      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627790/portfolio/weatherman.stephinreji.me__Moto_G4_neiw2k.png"
     },
     4: {
-      title: "PipBoy||Made using Bootstrap",
-      description: "This is the main screen of a PipBoy from fallout4",
-      link: "https://pipboy.stephinreji.me/"
+      title: "Photographer's Portfolio||Made using HTML/CSS",
+      description: "It's a fully customised Photographer portfolio",
+      projLink: "https://chitrahaar.stephinreji.me/",
+      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627857/portfolio/chitrahaar.stephinreji.me__Moto_G4_ckx7b2.png"
     },
     5: {
+      title: "PipBoy||Made using Bootstrap",
+      description: "This is the main screen of a PipBoy from fallout4",
+      projLink: "https://pipboy.stephinreji.me/",
+      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627956/portfolio/pipboy.stephinreji.me__custom-laptop_eywh6j.png"
+    },
+    6: {
       title: "Coming Soon||Made Using HTML/CSS/JS",
       description: "This is a coming soon page with an active countdown timer",
-      link: "https://timer.stephinreji.me/"
+      projLink: "https://timer.stephinreji.me/",
+      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604629091/portfolio/timer.stephinreji.me__Moto_G4_d4e2dv.png"
     },
   },
-  allIDs:[1,2,3,4,5]
+  allIDs:[1,2,3,4,5,6]
 
 }
 export default ProjectData
