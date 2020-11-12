e3ebaa31cbc4ad0a4cd3a22878b2c702d2adb237
stephin007 <stephinreji123@gmail.com>
2020-11-12 23:59:08 +0530

    Added DEV Badge

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,12 +14,13 @@ class Home extends Component{
                                     <h1>Stephin T Reji</h1>
                                     <p>I am a technically savvy customer success intern who is constantly striving for change and betterment</p>
                                     <div className="social-links">
-                                        <a href="https://github.com/stephin007" target="_blank" rel = "noopener noreferrer"><i className= "fa fa-github" /></a>
-                                        <a href="https://twitter.com/reji_stephin" target="_blank" rel = "noopener noreferrer"><i className= "fa fa-twitter" /></a>
-                                        <a href="https://wa.me/918860102173?text=Would%20love%20to%20talk%20with%20you" target="_blank" rel = "noopener noreferrer"><i className= "fa fa-whatsapp" /></a>
-                                        <a href="https://www.facebook.com/stephin.reji1998" target="_blank" rel = "noopener noreferrer"><i className= "fa fa-facebook" /></a>
-                                        <a href="https://www.linkedin.com/in/stephin-reji-a3439a186" target="_blank" rel = "noopener noreferrer"><i className= "fa fa-linkedin" /></a>
-                                        <a href="https://www.instagram.com/the_spectacled_one/?hl=en" target="_blank" rel = "noopener noreferrer"><i className= "fa fa-instagram"  /></a>
+                                        <a href="https://github.com/stephin007" target="_blank" rel = "noopener noreferrer"><i className= "fab fa-github" /></a>
+                                        <a href="https://dev.to/stephin007" aly="stephin007's DEV Profile"target="_blank" rel = "noopener noreferrer"><i class="fab fa-dev" ></i></a>
+                                        <a href="https://twitter.com/reji_stephin" target="_blank" rel = "noopener noreferrer"><i className= "fab fa-twitter" /></a>
+                                        <a href="https://wa.me/918860102173?text=Would%20love%20to%20talk%20with%20you" target="_blank" rel = "noopener noreferrer"><i className= "fab fa-whatsapp" /></a>
+                                        <a href="https://www.facebook.com/stephin.reji1998" target="_blank" rel = "noopener noreferrer"><i className= "fab fa-facebook" /></a>
+                                        <a href="https://www.linkedin.com/in/stephinreji" target="_blank" rel = "noopener noreferrer"><i className= "fab fa-linkedin" /></a>
+                                        <a href="https://www.instagram.com/the_spectacled_one/?hl=en" target="_blank" rel = "noopener noreferrer"><i className= "fab fa-instagram"  /></a>
                                     </div>
                                 </div>
                             </div>
