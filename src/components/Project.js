cf029e595c9f6384bb0f95ec02c704c7aecbb79d
stephin007 <stephinreji123@gmail.com>
2021-01-04 14:00:27 +0530

    Added Projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,45 +1,74 @@
-import React, {Component, Fragment} from 'react'
-import ProjectData from '../data/ProjectData'
+import React, { Component, Fragment } from "react";
+import ProjectData from "../data/ProjectData";
 
-import '../css/project.css'
+import "../css/project.css";
 
-class Portfolio extends Component{
-
-    render(){
-        return (
-            <Fragment>
-                <section className="portfolio section">
-                    <div className="container">
-                        <div className="row">
-                            <div className="section-title padd-15">
-                                <h2>Projects</h2>
-                            </div>
-                        </div>
-                        <div className="row">
-                            {
-                                ProjectData.allIDs.map((id, index) => (
-                                    <div className="portfolio-item padd-15" key={index} data-category="web-design" data-testid="portfolio-item">
-                                        <div className="portfolio-item-inner shadow-dark">
-                                            <div className="portfolio-img">
-                                                <img src={ProjectData.byID[id].imgLink} alt="portfolio-img"/>
-                                            </div>
-                                            <div className="portfolio-info">
-                                                <h4>{ProjectData.byID[id].title}</h4>
-                                                <p>{ProjectData.byID[id].description}</p>
-                                                <div className="icon">
-                                                <a href={ProjectData.byID[id].projLink} target="_blank" rel = "noopener noreferrer"><i className="fa fa-search"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
+class Portfolio extends Component {
+  render() {
+    return (
+      <Fragment>
+        <section className="portfolio section">
+          <div className="container">
+            <div className="row">
+              <div className="section-title padd-15">
+                <h2>Projects</h2>
+              </div>
+            </div>
+            <div className="row">
+              {ProjectData.allIDs.map((id, index) => (
+                <div
+                  className="portfolio-item padd-15"
+                  key={index}
+                  data-category="web-design"
+                  data-testid="portfolio-item"
+                >
+                  <div className="portfolio-item-inner shadow-dark">
+                    <div className="portfolio-img">
+                      <img
+                        src={ProjectData.byID[id].imgLink}
+                        alt="portfolio-img"
+                      />
                     </div>
-                </section>
-            </Fragment>
-            )
-        }
+                    <div className="portfolio-info">
+                      <h4>{ProjectData.byID[id].title}</h4>
+                      <p>{ProjectData.byID[id].description}</p>
+
+                      <div className="icon-github">
+                        <a
+                          href={ProjectData.byID[id].gitLink}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <i className="fab fa-github"></i>
+                        </a>
+                      </div>
+                      <div
+                        className="icon-search"
+                        style={{
+                          display:
+                            ProjectData.byID[id].projLink !== ""
+                              ? "block"
+                              : "none",
+                        }}
+                      >
+                        <a
+                          href={ProjectData.byID[id].projLink}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <i className="fa fa-search"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </Fragment>
+    );
+  }
 }
 
-export default Portfolio
+export default Portfolio;
--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,49 +1,71 @@
 const ProjectData = {
-  byID:{
+  byID: {
     1: {
-      title: "Live Cricket Application||Made using ReactJS",
-      description: "An App to get live Stats about cricket from around the world",
-      projLink: "https://criclive.stephinreji.me/",
-      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1606246612/portfolio/criclive.stephinreji.me__custom-laptop_dpriez.png"
+      title: "Marvel Heroes||Made using ReactJS",
+      description: "A Marvel app which shows first 100 characters",
+      projLink: "https://marvellll.web.app/",
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1609748664/portfolio/marvellll.web.app__custom-laptop_1_bmvupi.png",
+      gitLink: "https://github.com/stephin007/marvel",
     },
     2: {
+      title: "Stephin's Portfolio||Made using ReactJS",
+      description: "My Portfolio",
+      projLink: "https://stephinreji.me",
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1609748174/portfolio/Screenshot_from_2021-01-04_13-44-00_zzugxt.png",
+      gitLink: "https://github.com/stephin007/myportfolio",
+    },
+    3: {
+      title: "Live Cricket Application||Made using ReactJS",
+      description:
+        "An App to get live Stats about cricket from around the world",
+      projLink: "https://criclive.stephinreji.me/",
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1606246612/portfolio/criclive.stephinreji.me__custom-laptop_dpriez.png",
+      gitLink: "",
+    },
+    4: {
       title: "The Git Searcher||Made using ReactJS",
       description: "An App to search Github Users",
       projLink: "https://gitsearcher.stephinreji.me/",
-      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627671/portfolio/gitsearcher.stephinreji.me__Pixel_2_ccp0ai.png"
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1604627671/portfolio/gitsearcher.stephinreji.me__Pixel_2_ccp0ai.png",
+      gitLink: "",
     },
-    3: {
+    5: {
       title: "A Currency Convertor App||Made using ReactJS",
       description: "App fetches the currency exchange rates of 33 countries",
       projLink: "https://currencyconvertor.stephinreji.me/",
-      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627740/portfolio/currencyconvertor.stephinreji.me__Moto_G4_v5xir7.png"
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1604627740/portfolio/currencyconvertor.stephinreji.me__Moto_G4_v5xir7.png",
+      gitLink: "",
     },
-    4: {
+    6: {
       title: "A Weather Application||Made using ReactJS",
       description: "It uses an Open Weather API to fetch weather details",
       projLink: "https://weatherman.stephinreji.me/",
-      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627790/portfolio/weatherman.stephinreji.me__Moto_G4_neiw2k.png"
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1604627790/portfolio/weatherman.stephinreji.me__Moto_G4_neiw2k.png",
+      gitLink: "",
     },
-    5: {
+    7: {
       title: "Photographer's Portfolio||Made using HTML/CSS",
       description: "It's a fully customised Photographer portfolio",
       projLink: "https://chitrahaar.stephinreji.me/",
-      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627857/portfolio/chitrahaar.stephinreji.me__Moto_G4_ckx7b2.png"
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1604627857/portfolio/chitrahaar.stephinreji.me__Moto_G4_ckx7b2.png",
+      gitLink: "",
     },
-    6: {
+    8: {
       title: "PipBoy||Made using Bootstrap",
       description: "This is the main screen of a PipBoy from fallout4",
       projLink: "https://pipboy.stephinreji.me/",
-      imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604627956/portfolio/pipboy.stephinreji.me__custom-laptop_eywh6j.png"
-    }
-    // 7: {
-    //   title: "Coming Soon||Made Using HTML/CSS/JS",
-    //   description: "This is a coming soon page with an active countdown timer",
-    //   projLink: "https://timer.stephinreji.me/",
-    //   imgLink: "https://res.cloudinary.com/stephin/image/upload/v1604629091/portfolio/timer.stephinreji.me__Moto_G4_d4e2dv.png"
-    // },
+      imgLink:
+        "https://res.cloudinary.com/stephin/image/upload/v1604627956/portfolio/pipboy.stephinreji.me__custom-laptop_eywh6j.png",
+      gitLink: "",
+    },
   },
-  allIDs:[1,2,3,4,5,6]
-
-}
+  allIDs: [1, 2, 3, 4, 5, 6, 7, 8],
+};
 export default ProjectData
