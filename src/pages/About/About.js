401b459f8eaab57807bae7aa6c9a9c4e8ff2c7fa
Stephin Reji <stephinreji123@gmail.com>
2021-06-19 12:11:45 +0530

    converted class components to functional components (#26)

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,234 +1,229 @@
-import React, { Component } from "react";
+import React from "react";
 import AboutData from "../../data/AboutData";
 import ResumeButton from "../../library/components/ResumeButton";
 
 import "./about.css";
 
-class About extends Component {
-  render() {
-    return (
-      <section className="about section">
-        <div className="container">
-          <div className="row">
-            <div className="section-title padd-15">
-              <h2>About Me</h2>
-            </div>
+const About = () => {
+  return (
+    <section className="about section">
+      <div className="container">
+        <div className="row">
+          <div className="section-title padd-15">
+            <h2>About Me</h2>
           </div>
-          <div className="row">
-            <div className="about-content padd-15">
-              <div className="row">
-                <div className="about-text padd-15 ">
-                  <h3>
-                    Communication is the <span>key</span>
+        </div>
+        <div className="row">
+          <div className="about-content padd-15">
+            <div className="row">
+              <div className="about-text padd-15 ">
+                <h3>
+                  Communication is the <span>key</span>
+                </h3>
+                <p>{AboutData.aboutTxt}</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="personal-info padd-15">
+                <h3 className="pi-title">Personal Information</h3>
+                <div className="row">
+                  <div className="info-item padd-15">
+                    <p>
+                      Birthday :{" "}
+                      <span>{AboutData.personalInformation.birthday}</span>
+                    </p>
+                  </div>
+                  <div className="info-item padd-15">
+                    <p>
+                      Age : <span>{AboutData.personalInformation.age}</span>
+                    </p>
+                  </div>
+                  <div className="info-item padd-15">
+                    <p>
+                      Email :{" "}
+                      <span>
+                        <a href="mailto:{AboutData.personalInformation.email}">
+                          {AboutData.personalInformation.email}
+                        </a>
+                      </span>
+                    </p>
+                  </div>
+                  <div className="info-item padd-15">
+                    <p>
+                      Degree :{" "}
+                      <span>{AboutData.personalInformation.degree}</span>
+                    </p>
+                  </div>
+                  <div className="info-item padd-15">
+                    <p>
+                      City : <span>{AboutData.personalInformation.city}</span>
+                    </p>
+                  </div>
+                  <div className="info-item padd-15" id="language">
+                    <p>
+                      Languages Known :{" "}
+                      <span>
+                        {AboutData.personalInformation.languages.map(
+                          (language) => language + ", "
+                        )}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="buttons padd-15">
+                    <ResumeButton buttonText="Download Resume" />
+                    <a
+                      href={require(`../../assets/resume.pdf`)}
+                      className="bn"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      View Resume
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="skills padd-15">
+                <div className="row">
+                  <h3 className="pi-title padd-15">Interpersonal Skills</h3>
+                  {AboutData.interpersonalSkillIDs.map((id) => (
+                    <div className="skill-item padd-15">
+                      <h5>{id}</h5>
+                      <div className="progress">
+                        <div
+                          className="progress-in"
+                          style={{
+                            width: `${AboutData.interpersonalSkills[id]}%`,
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="skills padd-15" id="computerskills">
+                <div className="row">
+                  <h3 className="pi-title padd-15">Computer Skills</h3>
+                  {AboutData.computerSkillIDs.map((id) => (
+                    <div className="skill-item padd-15">
+                      <h5>{id}</h5>
+                      <div className="progress">
+                        <div
+                          className="progress-in"
+                          style={{
+                            width: `${AboutData.computerSkills[id]}%`,
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="skills padd-15" id="tools">
+                <div className="row">
+                  <h3 className="pi-title padd-15">
+                    Languages/Tools/Libraries
                   </h3>
-                  <p>{AboutData.aboutTxt}</p>
+                  {AboutData.languageSkillIDs.map((id) => (
+                    <div className="skill-item padd-15">
+                      <h5>{id}</h5>
+                      <div className="progress">
+                        <div
+                          className="progress-in"
+                          style={{
+                            width: `${AboutData.languageSkills[id]}%`,
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-              <div className="row">
-                <div className="personal-info padd-15">
-                  <h3 className="pi-title">Personal Information</h3>
-                  <div className="row">
-                    <div className="info-item padd-15">
-                      <p>
-                        Birthday :{" "}
-                        <span>{AboutData.personalInformation.birthday}</span>
-                      </p>
-                    </div>
-                    <div className="info-item padd-15">
-                      <p>
-                        Age : <span>{AboutData.personalInformation.age}</span>
-                      </p>
-                    </div>
-                    <div className="info-item padd-15">
-                      <p>
-                        Email :{" "}
-                        <span>
-                          <a href="mailto:{AboutData.personalInformation.email}">
-                            {AboutData.personalInformation.email}
-                          </a>
-                        </span>
-                      </p>
-                    </div>
-                    <div className="info-item padd-15">
-                      <p>
-                        Degree :{" "}
-                        <span>{AboutData.personalInformation.degree}</span>
-                      </p>
-                    </div>
-                    <div className="info-item padd-15">
-                      <p>
-                        City : <span>{AboutData.personalInformation.city}</span>
-                      </p>
-                    </div>
-                    <div className="info-item padd-15" id="language">
-                      <p>
-                        Languages Known :{" "}
-                        <span>
-                          {AboutData.personalInformation.languages.map(
-                            (language) => language + ", "
-                          )}
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="buttons padd-15">
-                      <ResumeButton buttonText="Download Resume" />
-                      <a
-                        href={require(`../../assets/resume.pdf`)}
-                        className="bn"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        View Resume
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="skills padd-15">
-                  <div className="row">
-                    <h3 className="pi-title padd-15">Interpersonal Skills</h3>
-                    {AboutData.interpersonalSkillIDs.map((id) => (
-                      <div className="skill-item padd-15">
-                        <h5>{id}</h5>
-                        <div className="progress">
-                          <div
-                            className="progress-in"
-                            style={{
-                              width: `${AboutData.interpersonalSkills[id]}%`,
-                            }}
-                          ></div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div className="skills padd-15" id="computerskills">
-                  <div className="row">
-                    <h3 className="pi-title padd-15">Computer Skills</h3>
-                    {AboutData.computerSkillIDs.map((id) => (
-                      <div className="skill-item padd-15">
-                        <h5>{id}</h5>
-                        <div className="progress">
-                          <div
-                            className="progress-in"
-                            style={{
-                              width: `${AboutData.computerSkills[id]}%`,
-                            }}
-                          ></div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div className="skills padd-15" id="tools">
-                  <div className="row">
-                    <h3 className="pi-title padd-15">
-                      Languages/Tools/Libraries
-                    </h3>
-                    {AboutData.languageSkillIDs.map((id) => (
-                      <div className="skill-item padd-15">
-                        <h5>{id}</h5>
-                        <div className="progress">
-                          <div
-                            className="progress-in"
-                            style={{
-                              width: `${AboutData.languageSkills[id]}%`,
-                            }}
-                          ></div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="experience padd-15">
-                  <h3 className="title">Work Experience</h3>
-                  <div className="row">
-                    <div className="timeline-box padd-15">
-                      <div className="timeline shadow-dark">
-                        {AboutData.workExperienceIDs.map((id) => (
-                          <div className="timeline-item">
-                            <div className="circle-dot" />
-                            <h6 className="timeline-date">
-                              <i className="fa fa-calendar"></i>
-                              {AboutData.workExperience[id].timeline}
-                            </h6>
-                            <h4 className="timeline-title">
-                              {AboutData.workExperience[id].company}
-                              <a
-                                href={AboutData.workExperience[id].website}
-                                className="company-website"
+            </div>
+            <div className="row">
+              <div className="experience padd-15">
+                <h3 className="title">Work Experience</h3>
+                <div className="row">
+                  <div className="timeline-box padd-15">
+                    <div className="timeline shadow-dark">
+                      {AboutData.workExperienceIDs.map((id) => (
+                        <div className="timeline-item">
+                          <div className="circle-dot" />
+                          <h6 className="timeline-date">
+                            <i className="fa fa-calendar"></i>
+                            {AboutData.workExperience[id].timeline}
+                          </h6>
+                          <h4 className="timeline-title">
+                            {AboutData.workExperience[id].company}
+                            <a
+                              href={AboutData.workExperience[id].website}
+                              className="company-website"
+                            >
+                              <i class="fab fa-chrome"></i>
+                            </a>
+                          </h4>
+                          <p className="timeline-text">
+                            <span>
+                              Job Title: {AboutData.workExperience[id].jobTitle}
+                            </span>{" "}
+                            <br />
+                            <div className="underline">Work Responsibility</div>
+                            <br />
+                            {AboutData.workExperience[id].responsibilities !==
+                            "" ? (
+                              <>
+                                {AboutData.workExperience[
+                                  id
+                                ].responsibilities.map((responsibility) => (
+                                  <>
+                                    - {responsibility}
+                                    <br />
+                                  </>
+                                ))}
+                              </>
+                            ) : (
+                              <h2
+                                style={{
+                                  textAlign: "center",
+                                  padding: "40px",
+                                  marginBottom: "-40px",
+                                  color: "lightgrey",
+                                }}
                               >
-                                <i class="fab fa-chrome"></i>
-                              </a>
-                            </h4>
-                            <p className="timeline-text">
-                              <span>
-                                Job Title:{" "}
-                                {AboutData.workExperience[id].jobTitle}
-                              </span>{" "}
-                              <br />
-                              <div className="underline">
-                                Work Responsibility
-                              </div>
-                              <br />
-                              {AboutData.workExperience[id].responsibilities !==
-                              "" ? (
-                                <>
-                                  {AboutData.workExperience[
-                                    id
-                                  ].responsibilities.map((responsibility) => (
-                                    <>
-                                      - {responsibility}
-                                      <br />
-                                    </>
-                                  ))}
-                                </>
-                              ) : (
-                                <h2
-                                  style={{
-                                    textAlign: "center",
-                                    padding: "40px",
-                                    marginBottom: "-40px",
-                                    color: "lightgrey",
-                                  }}
-                                >
-                                  To be Added
-                                </h2>
-                              )}
-                            </p>
-                          </div>
-                        ))}
-                      </div>
+                                To be Added
+                              </h2>
+                            )}
+                          </p>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
-                <div className="education padd-15">
-                  <h3 className="title" data-testid="section--eductaion">
-                    Education
-                  </h3>
-                  <div className="row">
-                    <div className="timeline-box padd-15">
-                      <div className="timeline shadow-dark">
-                        {/* Timeline-item start */}
-                        {AboutData.educationIDs.map((id) => (
-                          <div className="timeline-item">
-                            <div className="circle-dot" />
-                            <h6 className="timeline-date">
-                              <i className="fa fa-calendar"></i>{" "}
-                              {AboutData.education[id].timeline}
-                            </h6>
-                            <h4 className="timeline-title">
-                              {AboutData.education[id].title}
-                            </h4>
-                            <p className="timeline-text">
-                              {AboutData.education[id].institute}
-                            </p>
-                          </div>
-                        ))}
-                      </div>
+              </div>
+              <div className="education padd-15">
+                <h3 className="title" data-testid="section--eductaion">
+                  Education
+                </h3>
+                <div className="row">
+                  <div className="timeline-box padd-15">
+                    <div className="timeline shadow-dark">
+                      {/* Timeline-item start */}
+                      {AboutData.educationIDs.map((id) => (
+                        <div className="timeline-item">
+                          <div className="circle-dot" />
+                          <h6 className="timeline-date">
+                            <i className="fa fa-calendar"></i>{" "}
+                            {AboutData.education[id].timeline}
+                          </h6>
+                          <h4 className="timeline-title">
+                            {AboutData.education[id].title}
+                          </h4>
+                          <p className="timeline-text">
+                            {AboutData.education[id].institute}
+                          </p>
+                        </div>
+                      ))}
                     </div>
                   </div>
                 </div>
@@ -236,9 +231,9 @@ class About extends Component {
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 export default About;
--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -1,106 +1,93 @@
-import React, { Fragment, Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import "./navbar.css";
 
-class Navbar extends Component {
-  state = {
-    isOpen: false,
+const Navbar = ({ toggleTheme, theme }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(true);
   };
 
-  handleClick = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
+  const closeNav = () => {
+    setIsOpen(false);
   };
 
-  closeNav = () => {
-    this.setState({
-      isOpen: false,
-    });
-  };
-
-  render() {
-    const { toggleTheme, theme } = this.props;
-
-    return (
-      <Fragment>
-        <nav>
-          <div className="logobtn">
-            <div className="logo">
-              <Link
-                to="/"
-                onClick={this.closeNav}
-                aria-label="Homepage"
-                alt="Homepage"
-              >
-                <img
-                  src="https://res.cloudinary.com/stephin/image/upload/v1604631468/portfolio/download_cvd0q0.png"
-                  alt="logo"
-                />
-              </Link>
-            </div>
-            <div className="mob">
+  return (
+    <>
+      <nav>
+        <div className="logobtn">
+          <div className="logo">
+            <Link
+              to="/"
+              onClick={() => closeNav}
+              aria-label="Homepage"
+              alt="Homepage"
+            >
               <img
-                src={theme === "light" ? moon : sun}
-                onClick={toggleTheme}
-                alt="toggle"
-                className="sun-moon-1"
+                src="https://res.cloudinary.com/stephin/image/upload/v1604631468/portfolio/download_cvd0q0.png"
+                alt="logo"
               />
-              <button
-                aria-label="Main menu"
-                className="btn"
-                onClick={this.handleClick}
-              >
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-              </button>
-            </div>
+            </Link>
           </div>
-
-          <ul
-            className={this.state.isOpen ? "showNav" : "undefined"}
-            data-testid="navbar"
-          >
-            <li>
-              <Link to="/" onClick={this.closeNav} alt="home">
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" onClick={this.closeNav} alt="about">
-                ABOUT
-              </Link>
-            </li>
-            <li>
-              <Link to="/projects" onClick={this.closeNav} alt="project">
-                PROJECTS
-              </Link>
-            </li>
-            <li>
-              <a
-                href="https://blog.stephinreji.me"
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={this.closeNav}
-                alt="project"
-              >
-                BLOG
-              </a>
-            </li>
+          <div className="mob">
             <img
               src={theme === "light" ? moon : sun}
               onClick={toggleTheme}
               alt="toggle"
-              className="sun-moon-2"
+              className="sun-moon-1"
             />
-          </ul>
-        </nav>
-      </Fragment>
-    );
-  }
-}
+            <button
+              aria-label="Main menu"
+              className="btn"
+              onClick={() => handleClick}
+            >
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </button>
+          </div>
+        </div>
+
+        <ul className={isOpen ? "showNav" : "undefined"} data-testid="navbar">
+          <li>
+            <Link to="/" onClick={() => closeNav} alt="home">
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={() => closeNav} alt="about">
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" onClick={() => closeNav} alt="project">
+              PROJECTS
+            </Link>
+          </li>
+          <li>
+            <a
+              href="https://blog.stephinreji.me"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => closeNav}
+              alt="project"
+            >
+              BLOG
+            </a>
+          </li>
+          <img
+            src={theme === "light" ? moon : sun}
+            onClick={toggleTheme}
+            alt="toggle"
+            className="sun-moon-2"
+          />
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
