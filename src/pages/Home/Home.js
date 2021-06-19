296ba7b915dfe9258142793afebf5715d54976e6
stephin007 <stephinreji123@gmail.com>
2021-06-19 12:10:41 +0530

    converted class components to functional components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,48 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import SocialButton from "../../library/components/SocialButton";
 
 import SocialLinks from "../../data/SocialLinks";
 import "./home.css";
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="main-container">
-        <div className="main-content">
-          <section className="home section">
-            <div className="container">
-              <div className="intro">
-                <img
-                  src="https://res.cloudinary.com/stephin/image/upload/v1609764633/portfolio/WhatsApp_Image_2021-01-04_at_18.19.09_gxp5cg.jpg"
-                  alt="Profile Img"
-                  className="shadow-img"
-                />
-                <h1>Stephin T Reji</h1>
-                <p>
-                  I am a technically savvy Customer Success Analyst who is
-                  constantly striving for change and betterment
-                </p>
-                <div className="social-links">
-                  {SocialLinks.map((Sociallink, index) => {
-                    const { link, classes } = Sociallink;
-                    return (
-                      <>
-                        <SocialButton
-                          socialLink={link}
-                          iconClass={classes}
-                          key={index}
-                        />
-                      </>
-                    );
-                  })}
-                </div>
+const Home = () => {
+  return (
+    <div className="main-container">
+      <div className="main-content">
+        <section className="home section">
+          <div className="container">
+            <div className="intro">
+              <img
+                src="https://res.cloudinary.com/stephin/image/upload/v1609764633/portfolio/WhatsApp_Image_2021-01-04_at_18.19.09_gxp5cg.jpg"
+                alt="Profile Img"
+                className="shadow-img"
+              />
+              <h1>Stephin T Reji</h1>
+              <p>
+                I am a technically savvy Customer Success Analyst who is
+                constantly striving for change and betterment
+              </p>
+              <div className="social-links">
+                {SocialLinks.map((Sociallink, index) => {
+                  const { link, classes } = Sociallink;
+                  return (
+                    <>
+                      <SocialButton
+                        socialLink={link}
+                        iconClass={classes}
+                        key={index}
+                      />
+                    </>
+                  );
+                })}
               </div>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,82 +1,80 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
 import ProjectData from "../../data/ProjectData";
 
 import "./project.css";
 
-class Portfolio extends Component {
-  render() {
-    return (
-      <Fragment>
-        <section className="portfolio section">
-          <div className="container">
-            <div className="row">
-              <div className="section-title padd-15">
-                <h2>Projects</h2>
-              </div>
+const Portfolio = () => {
+  return (
+    <>
+      <section className="portfolio section">
+        <div className="container">
+          <div className="row">
+            <div className="section-title padd-15">
+              <h2>Projects</h2>
             </div>
-            <div className="row">
-              {ProjectData.allIDs.map((id, index) => (
-                <div
-                  className="portfolio-item padd-15"
-                  key={index}
-                  data-category="web-design"
-                  data-testid="portfolio-item"
-                >
-                  <div className="portfolio-item-inner shadow-dark">
-                    <div className="portfolio-img">
-                      <img
-                        src={ProjectData.byID[id].imgLink}
-                        alt="portfolio-img"
-                      />
-                    </div>
-                    <div className="portfolio-info">
-                      <h4>{ProjectData.byID[id].title}</h4>
-                      <p>{ProjectData.byID[id].description}</p>
+          </div>
+          <div className="row">
+            {ProjectData.allIDs.map((id, index) => (
+              <div
+                className="portfolio-item padd-15"
+                key={index}
+                data-category="web-design"
+                data-testid="portfolio-item"
+              >
+                <div className="portfolio-item-inner shadow-dark">
+                  <div className="portfolio-img">
+                    <img
+                      src={ProjectData.byID[id].imgLink}
+                      alt="portfolio-img"
+                    />
+                  </div>
+                  <div className="portfolio-info">
+                    <h4>{ProjectData.byID[id].title}</h4>
+                    <p>{ProjectData.byID[id].description}</p>
 
-                      <div
-                        className="icon-github"
-                        style={{
-                          display:
-                            ProjectData.byID[id].gitLink !== ""
-                              ? "block"
-                              : "none",
-                        }}
+                    <div
+                      className="icon-github"
+                      style={{
+                        display:
+                          ProjectData.byID[id].gitLink !== ""
+                            ? "block"
+                            : "none",
+                      }}
+                    >
+                      <a
+                        href={ProjectData.byID[id].gitLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
                       >
-                        <a
-                          href={ProjectData.byID[id].gitLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <i className="fab fa-github"></i>
-                        </a>
-                      </div>
-                      <div
-                        className="icon-search"
-                        style={{
-                          display:
-                            ProjectData.byID[id].projLink !== ""
-                              ? "block"
-                              : "none",
-                        }}
+                        <i className="fab fa-github"></i>
+                      </a>
+                    </div>
+                    <div
+                      className="icon-search"
+                      style={{
+                        display:
+                          ProjectData.byID[id].projLink !== ""
+                            ? "block"
+                            : "none",
+                      }}
+                    >
+                      <a
+                        href={ProjectData.byID[id].projLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
                       >
-                        <a
-                          href={ProjectData.byID[id].projLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <i className="fa fa-search"></i>
-                        </a>
-                      </div>
+                        <i className="fa fa-search"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-        </section>
-      </Fragment>
-    );
-  }
-}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Portfolio;
